c9d77974d2d736837f7930a9ea832af33b3c976c
bianca <biancasousa439@gmail.com>
2023-02-20 19:43:42 -0300

    add functions equalizacao, filtro_mediana, segmentar

--- cross_validation/utils/pre_processed.py
+++ cross_validation/utils/pre_processed.py
@@ -1,16 +1,37 @@
 import numpy as np
-from skimage.filters import threshold_otsu
+from skimage.filters import threshold_otsu, median
+from skimage.util import invert
+from skimage.exposure import equalize_hist
 
-def alargamento(img, k, e):
-    imgAl = np.zeros(img.shape)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            imgAl[i][j] = 1/(1 + (k/img[i][j])**e)
-    return imgAl
+def equalizacao(imagem):
+    img_eq = np.zeros_like(imagem)
+
+    if len(imagem.shape) == 3:
+        for d in range(imagem.shape[2]):
+            img_eq[:,:,d] = equalize_hist(imagem[:,:,d])
+
+    else:
+        img_eq = equalize_hist(imagem)
+
+    return img_eq
+
+
+def filtro_mediana(imagem):
+    img_mediana = np.zeros_like(imagem)
+    for d in range(imagem.shape[2]):
+        img_mediana[:,:,d] = median((imagem[:,:,d]*255).astype(np.uint8))/255.0 
+    return img_mediana
+
 
 def otsu(img):
-    thresh = threshold_otsu(img)
-    binary = img > thresh
-    return binary
+    thresh = threshold_otsu(img[:,:,2])
+    mask = img[:,:,2] > thresh
+    return invert(mask)
 
+
+def segmentar(imagem, mask):
+    img_seg = imagem.copy()
+    img_seg[~mask] = 0
+    return img_seg
+    
 
