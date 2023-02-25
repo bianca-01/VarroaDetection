6ad16e1206b640ec1a90945b8f0ba417c9ddfc1c
bianca <biancasousa439@gmail.com>
2023-02-24 21:47:55 -0300

    update images

--- cross_validation/utils/images.py
+++ cross_validation/utils/images.py
@@ -1,8 +1,8 @@
+import numpy as np
 from tqdm.notebook import tqdm_notebook
-
-from skimage.io import imread_collection
-
-from utils.pre_processed import equalizacao, filtro_mediana, otsu, segmentar
+from skimage.io import imread
+from utils.pre_processed import equalizacao, otsu, segmentar, filtro_mediana
+from utils.feature_extraction import extract_features, pca
 
 class Images:
     def __init__(self, metadata):
@@ -11,22 +11,23 @@ class Images:
         self.labels = metadata['label'].to_numpy()
         self.imgs_normalized = []
         self.imgs_equalized = []
-        self.imgs_processed = []
         self.masks = []
         self.imgs_segmented = []
         self.feats = {}
+        self.pca = {}
         self.load()
     
 
     def load(self):
-        self.imgs = imread_collection(self.path)
-
+        self.imgs = [imread(path) for path in self.path]
+        
         saudaveis = len(self.labels[self.labels == 0])
         infectadas = len(self.labels[self.labels == 1])
         
         print(f'{len(self.imgs)} imagens carregadas')
         print(f'{infectadas} infectadas')
         print(f'{saudaveis} saudáveis')
+        
 
     def normalize(self):
         for img in tqdm_notebook(self.imgs):
@@ -39,18 +40,35 @@ class Images:
             img_equalized = equalizacao(img)
             self.imgs_equalized.append(img_equalized)
             
-    def filter_median(self):
-        for img in tqdm_notebook(self.imgs_equalized):
-            img_processed = filtro_mediana(img)
-            self.imgs_processed.append(img_processed)
-
 
     def segment(self):
-        for img in tqdm_notebook(self.imgs_processed):
+        for img in tqdm_notebook(self.imgs_equalized):
             mask = otsu(img)
             img_segmented = segmentar(img, mask)
             self.masks.append(mask)
             self.imgs_segmented.append(img_segmented)
+
+    
+    def extraction(self, descritor='GLCM', op=1):
+        if op == 1:
+            imgs = self.imgs_segmented
+            index = f'{descritor}_segmented'
+
+        elif op == 2:
+            imgs = self.imgs_equalized
+            index = f'{descritor}_equalized'
+        
+        self.feats[index] = extract_features(descritor, imgs)
+
+
+    def run_pca(self):
+        components = [3, 5, 7, 9]
+        for comp in tqdm_notebook(components):
+            for key in tqdm_notebook(self.feats.keys()):
+                self.pca[f'{key}_pca_{comp}'] = pca(self.feats[key], comp)
+
+    
+    
 
 
     
